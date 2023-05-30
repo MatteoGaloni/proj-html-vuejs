50454b591c8406979709b8611e4d8058a9b9b44f
MatteoGaloni <mtgaloni@gmail.com>
2023-05-30 11:48:23 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+
+export default {
+  components: {
+    HelloWorld,
+  }
+}
+
+</script>
+
+<template>
+  <h1>CIAO</h1>
+  <HelloWorld />
+</template>
+
+<style scoped lang="scss"></style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+  name: "HelloWorld"
+}
+
+</script>
+
+<template>
+  <div class="test">
+    <h1>HELLO VUE TEST</h1>
+    <p>test</p>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.test {
+  h1 {
+    color: red;
+  }
+}
+</style>
